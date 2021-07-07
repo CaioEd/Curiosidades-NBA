1fd376cac23fb7ac4eb78c959b8b49e8ebfbaf82
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2021-07-07 16:05:55 -0300

    Update JS

--- code/scripts.js
+++ code/scripts.js
@@ -1,19 +1,43 @@
-function question1() {
+function question1(){
     const res = document.querySelector('#res')
+    const q1 = prompt('Digite sua resposta:')
 
-    const buttons = document.querySelectorAll('.alternativa')
-
-    const alternativas = [
-        '16',
-        '20',
-        '17',
-        '10'
-    ]
-
-
-    if(buttons.value == alternativas[2]){
-        res.innerHTML = (`Você acertou`)
+    if(q1 == '17' || q1 == 'dezessete' || q1 == 'Dezessete' || q1 == 'DEZESSETE'){
+        res.innerHTML = (`<h1>Você acertou</h1>  <p>Vá para próxima pergunta</p>`)
     } else{
-        res.innerHTML = (`Você errou`)
+        res.innerHTML = (`<p><strong>Você errou</strong> <br> Tente novamente ou vá para próxima pergunta</p>`)
+    }
+}
+
+function question2(){
+    const res = document.querySelector('#res')
+    const q2 = prompt('Digite sua resposta:')
+
+    if(q2 == '17' || q2 == 'dezessete' || q2 == 'Dezessete' || q2 == 'DEZESSETE'){
+        res.innerHTML = (`<h1>Você acertou</h1>  <p>Vá para próxima pergunta</p>`)
+    } else{
+        res.innerHTML = (`<p><strong>Você errou</strong> <br> Tente novamente ou vá para próxima pergunta</p>`)
+    }
+}
+
+function question3(){
+    const res = document.querySelector('#res')
+    const q1 = prompt('Digite sua resposta:')
+
+    if(q1 == '17' || q1 == 'dezessete' || q1 == 'Dezessete' || q1 == 'DEZESSETE'){
+        res.innerHTML = (`<h1>Você acertou</h1>  <p>Vá para próxima pergunta</p>`)
+    } else{
+        res.innerHTML = (`<p><strong>Você errou</strong> <br> Tente novamente ou vá para próxima pergunta</p>`)
+    }
+}
+
+function question4(){
+    const res = document.querySelector('#res')
+    const q1 = prompt('Digite sua resposta:')
+
+    if(q1 == '17' || q1 == 'dezessete' || q1 == 'Dezessete' || q1 == 'DEZESSETE'){
+        res.innerHTML = (`<h1>Você acertou</h1>  <p>Vá para próxima pergunta</p>`)
+    } else{
+        res.innerHTML = (`<p><strong>Você errou</strong> <br> Tente novamente ou vá para próxima pergunta</p>`)
     }
 }
