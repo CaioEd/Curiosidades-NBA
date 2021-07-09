9978b653ed5b3266450729f0d303ce7801ca1a9f
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2021-07-09 15:23:13 -0300

    Update JS, HTML, CSS

--- code/scripts.js
+++ code/scripts.js
@@ -5,25 +5,15 @@ function responder(){
     const respostasCorretas = [
         '17',
         'Abdul-Jabbar',
-        'Bill Russell'
+        'Bill Russell',
+        'Warriors 15/16'
     ]
-
-    if(select.value == respostasCorretas[0] || select.value == respostasCorretas[1] || select.value == respostasCorretas[2]){
-        res.innerHTML = (`<h1>Você acertou</h1> <strong><p>Parabéns</p></strong>  <p>Vá para próxima pergunta</p>`)
+    
+    if(select.value == respostasCorretas[3]){
+        res.innerHTML = (`<h1>Você acertou</h1> <strong><h2>Parabéns</h2></strong>  <p>Se você não foi bem no quiz, tente novamente clicando no botão abaixo.</p>`)
+    } else if(select.value == respostasCorretas[0] || select.value == respostasCorretas[1] || select.value == respostasCorretas[2]){
+        res.innerHTML = (`<h1>Você acertou</h1> <strong><h2>Parabéns</h2></strong>  <p>Vá para próxima pergunta</p>`)
     } else{
         res.innerHTML = (`<h1>Você errou</h1> <br> <p>Tente novamente ou vá para próxima pergunta</p>`)
-    }
-}
-
-function finalizar(){
-    const res = document.querySelector('#res')
-    const select = document.querySelector('#alternativas')
-
-    const correta = 'Warriors 15/16'
-
-    if(select.value == correta){
-        res.innerHTML = (`<h1>Você acertou</h1> <strong><p>Parabéns</p></strong>  <p>Se você não foi bem no quiz, tente novamente clicando no botão abaixo.</p>`)
-    } else{
-        res.innerHTML = (`<h1>Você errou</h1> <br> <p>Tente novamente</p>`)
     }
 }
